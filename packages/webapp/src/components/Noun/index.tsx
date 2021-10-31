d41845e68c0c1d7da392aa91000c992480fef3d4
rayo <0xrayo.eth@gmail.com>
2021-10-31 18:12:02 -0500

    dynamically change noun ID

--- packages/webapp/src/components/Noun/index.tsx
+++ packages/webapp/src/components/Noun/index.tsx
@@ -5,6 +5,7 @@ import Image from 'react-bootstrap/Image';
 import { utils } from 'ethers';
 import { contract as SeederContract } from '../../wrappers/nounsSeeder';
 import { contract as DesciptorContract } from '../../wrappers/nounsDescriptor';
+import { contract as AuctionContract } from '../../wrappers/nounsAuction';
 import { provider } from '../../config';
 import { useAppDispatch } from '../../hooks';
 import { setActiveBackground } from '../../state/slices/background';
@@ -18,64 +19,56 @@ const Noun: React.FC<{ alt: string }> = props => {
   const dispatch = useAppDispatch();
   const [img, setImg] = useState('');
 
+  let lockBlockNumber = -1;
+
+  async function getNextNounId() {
+    const res =  await AuctionContract.auction();
+    const nounNumber = utils.bigNumberify(res.nounId).toNumber();
+    return nounNumber + 1;
+    
+  }
+
   function subBlocks() {
     provider.on('block', (blockNumber) => {
-      generateNoun(91);
+      console.log("new block: ", blockNumber);
+      if(blockNumber !== lockBlockNumber) {
+        lockBlockNumber = blockNumber;
+        generateNoun();
+      }
     });
   }
 
-  function subAuction() {
-    console.log("looking for events..");
-    const filter = {
-      address: "0xF15a943787014461d94da08aD4040f79Cd7c124e",
-      topics: [
-        utils.id("AuctionCreated(uint256, uint256, uint256)"),
-      ]
-    };
-
-    const filter2 = {
-      address: "0xF15a943787014461d94da08aD4040f79Cd7c124e",
-      topics: [
-        utils.id("AuctionExtended(uint256, uint256)"),
-      ]
-    };
-
-    provider.on(filter, (log, event) => {
-      console.log(log);
-      console.log(event)
-    });
-
-    provider.on(filter2, (log, event) => {
-      console.log(log);
-      console.log(event)
-    });
-  }
-
-  useEffect(()=> {
-    subAuction();
+  useEffect(() => {
     subBlocks();
   }, []);
 
-  const generateNoun = async (nounId: Number) => {
-      const seed = await SeederContract.generateSeed(
-        nounId,
-        DesciptorContract.address,
-        {
-          blockTag: 'pending'
-        }
-      );
-      const useGreyBg = seed[0] === 0;
-      dispatch(setActiveBackground(useGreyBg));
-      const svg = await DesciptorContract.generateSVGImage(seed);
-      setImg(svg);
+  const generateNoun = async () => {
+
+    const nounId = await getNextNounId();
+    console.log(nounId);
+    const isNounder = nounId % 10 === 0;
+    const nextNounId = isNounder ? nounId + 1 : nounId;
+    console.log("generating Noun for Id: ", nextNounId);
+    
+    const seed = await SeederContract.generateSeed(
+      nextNounId,
+      DesciptorContract.address,
+      {
+        blockTag: 'pending'
+      }
+    );
+    const useGreyBg = seed[0] === 0;
+    dispatch(setActiveBackground(useGreyBg));
+    const svg = await DesciptorContract.generateSVGImage(seed);
+    setImg(svg);
   }
   const { alt } = props;
   return (
     <div className={classes.imgWrapper}>
       {img && <Image className={classes.img} src={`data:image/svg+xml;base64,${img}`} alt={alt} fluid />}
-      {!img && <LoadingNoun/>}
+      {!img && <LoadingNoun />}
     </div>
-    );
+  );
 };
 
 export default Noun;
