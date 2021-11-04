67512428f4549ee3fbcbc3e63133e189da7cd5c0
theforager <0xforager@gmail.com>
2021-11-04 00:04:28 -0500

    Refactor sendmessage for new vote setup

--- packages/backend/sendmessage/app.js
+++ packages/backend/sendmessage/app.js
@@ -1,12 +1,8 @@
 // SPDX-License-Identifier: MIT-0
 
 /**
- * Example
- *    CLIENT A SENDS:
- *      {"action": "sendmessage", "data": "hello world"}
- * 
- *    CLIENT B RECEIVES:
- *      "hello world"
+ * Event Example:
+ *   {"action": "sendmessage", "nounId": "22", "blockhash": "a83d9e", "vote": "like"}
  * 
  */
 
@@ -14,25 +10,57 @@ const AWS = require('aws-sdk');
 
 const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
 
-exports.handler = async event => {
+/**
+ * Update the DB counter
+ * 
+ * @param {Object} data Data to update in the DB
+ * @returns {Integer} totalConnection count in the DB
+ */
+async function updateVote(dbKey, voteType) {
+  const updateParams = {
+    TableName: process.env.VOTE_TABLE_NAME,
+    Key: { nounIdWithBlockHash: dbKey },
+    ExpressionAttributeValues: { ':start': 0, ':incr': 1 },
+    UpdateExpression: `set ${voteType} = if_not_exists(${voteType}, :start) + :incr`,
+    ReturnValues: 'ALL_NEW'
+  };
+
+  try {
+    let newValues = await ddb.update(updateParams).promise();
+    return newValues.Attributes.totalConnected;
+  } catch (err) {
+    // If totalConnected was already updated, ignore error, otherwise throw
+    if (err.code !== "ConditionalCheckFailedException") {
+      throw err;
+    }
+  }
+}
+
+
+/**
+ * Distribute the data to each client
+ * 
+ * @param {Object} data Data to distribute to all clients
+ */
+async function distributeVote(voteType) {
+  let connectionCount = 0;
   let connectionData;
   
   try {
     connectionData = await ddb.scan({ TableName: process.env.TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
   } catch (e) {
-    return { statusCode: 500, body: e.stack };
+    throw e; //return { statusCode: 500, body: e.stack };
   }
   
   const apigwManagementApi = new AWS.ApiGatewayManagementApi({
     apiVersion: '2018-11-29',
-    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage  // TODO: FIX THIS REFERENCE
   });
-  
-  const postData = JSON.parse(event.body).data;
   
   const postCalls = connectionData.Items.map(async ({ connectionId }) => {
     try {
-      await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
+      await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: voteType }).promise();
+      connectionCount++;
     } catch (e) {
       if (e.statusCode === 410) {
         console.log(`Found stale connection, deleting ${connectionId}`);
@@ -46,8 +74,74 @@ exports.handler = async event => {
   try {
     await Promise.all(postCalls);
   } catch (e) {
-    return { statusCode: 500, body: e.stack };
+    throw e; // return { statusCode: 500, body: e.stack };
   }
 
-  return { statusCode: 200, body: 'Data sent.' };
+  return connectionCount;
+}
+
+
+/**
+ * Update the connection count in the DB 
+ * 
+ * @param {Integer} count Count of connections
+ */
+async function updateCount(dbKey, count) {
+  const updateParams = {
+    TableName: process.env.VOTE_TABLE_NAME,
+    Key: { nounIdWithBlockHash: dbKey },
+    ExpressionAttributeValues: { ':newCount': count },
+    ConditionExpression: 'attribute_not_exists(totalConnected)',
+    UpdateExpression: 'set totalConnected = :newCount'
+  };
+
+  try {
+    await ddb.update(updateParams).promise();
+  } catch (err) {
+    // If totalConnected was already updated, ignore error, otherwise throw
+    if (err.code !== "ConditionalCheckFailedException") {
+      throw err;
+    }
+  }
+}
+
+
+
+/**
+ * 
+ * @param {Object} event
+ *    event.body:
+ *      - nounId
+ *      - blockhash
+ *      - vote
+ */
+
+exports.handler = async event => {
+  const body = JSON.parse(event.body);
+  const dbKey = `${body.nounId}||${body.blockhash}`;
+  const vote = body.vote;
+
+  let dbCount;
+  try {
+    dbCount = await updateVote(dbKey, vote);
+  } catch(err) {
+    return { statusCode: 500, body: 'Error updating vote in DB.', message: err.stack };
+  }
+
+  let distributeCount;
+  try {
+    distributeCount = await distributeVote(vote);
+  } catch(err) {
+    return { statusCode: 500, body: 'Error distributing vote to clients.', message: err.stack };
+  }
+
+  if (!dbCount) { // Update iif dbCount was not present
+    try {
+      await updateCount(dbKey, distributeCount);
+    } catch(err) {
+      return { statusCode: 500, body: 'Error updating connection count.', message: err.stack };
+    }
+  }
+
+  return { statusCode: 200, body: 'Vote updated.' };
 };
