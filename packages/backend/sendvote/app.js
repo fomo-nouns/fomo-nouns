147b89df012c28ad281a80ab690e28bed4353198
theforager <0xforager@gmail.com>
2021-11-06 17:58:18 -0500

    Add settlement to sendVote

--- packages/backend/sendvote/app.js
+++ packages/backend/sendvote/app.js
@@ -7,6 +7,8 @@
  */
 
 const AWS = require('aws-sdk');
+const { hasWinningVotes } = require('./scoreVotes.js');
+const { submitSettlement } = require('./settlement.js');
 
 const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
 
@@ -27,7 +29,7 @@ async function updateVote(dbKey, voteType) {
 
   try {
     let newValues = await ddb.update(updateParams).promise();
-    return newValues.Attributes.totalConnected;
+    return newValues.Attributes;
   } catch (err) {
     // If totalConnected was already updated, ignore error, otherwise throw
     if (err.code !== "ConditionalCheckFailedException") {
@@ -108,6 +110,37 @@ async function updateCount(dbKey, count) {
 
 
 /**
+ * Launch settlement of the Nouns auction
+ * 
+ * @param {Integer} count Count of connections
+ */
+ async function launchSettlement(dbKey, blockhash) {
+  const updateParams = {
+    TableName: process.env.VOTE_TABLE_NAME,
+    Key: { nounIdWithBlockHash: dbKey },
+    ExpressionAttributeValues: { ':status': true },
+    ConditionExpression: 'attribute_not_exists(settled)',
+    UpdateExpression: 'set settled = :status'
+  };
+
+  try {
+    // Lock the DB to prevent duplicate settlement
+    await ddb.update(updateParams).promise();
+
+    // Submit and wait for settlement transaction
+    // await submitSettlement(blockhash);
+    console.log('Fake attempt to settle!');
+  } catch (err) {
+    // If settlement was already attempted, ignore
+    if (err.code !== "ConditionalCheckFailedException") {
+      throw err;
+    }
+  }
+}
+
+
+
+/**
  * 
  * @param {Object} event
  *    event.body:
@@ -125,11 +158,21 @@ exports.handler = async event => {
   const endpoint = `${context.domainName}/${context.stage}`;
 
   // Update the DB with the latest vote
-  let dbCount;
+  let newValues;
   try {
-    dbCount = await updateVote(dbKey, vote);
+    newValues = await updateVote(dbKey, vote);
   } catch(err) {
     return { statusCode: 500, body: 'Error updating vote in DB.', message: err.stack };
+  }
+
+  // Launch settlement if new values signal voting has won
+  if (!newValues.settled && hasWinningVotes(newValues)) {
+    try {
+      await launchSettlement(dbKey, body.blockhash);
+      newValues.settled = true;
+    } catch(err) {
+      return { statusCode: 500, body: 'Error settling.', message: err.stack };
+    }
   }
 
   // Distribute votes to clients
@@ -141,7 +184,7 @@ exports.handler = async event => {
   }
 
   // Update connection count if not present
-  if (!dbCount) {
+  if (!newValues.totalConnected) {
     try {
       await updateCount(dbKey, distributeCount);
     } catch(err) {
