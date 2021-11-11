8f601c939b744dc3011abdacf6ba0b9b3447701e
theforager <0xforager@gmail.com>
2021-11-11 14:47:49 -0600

    Parallelize private key pull for speed

--- packages/backend/sendvote/utils/getEthereumPrivateKeys.js
+++ packages/backend/sendvote/utils/getEthereumPrivateKeys.js
@@ -4,9 +4,18 @@
  * @param {SecretsManager} secretsManager Instance of AWS SDK Secrets Manager
  * @returns {String, String} Returns Alchemy API key and Executor Private Key
  */
-async function getEthereumPrivateKeys(smc) {
-  let alchemyKey = await smc.getSecretValue({SecretId: 'nouns/AlchemyKey'}).promise();
-  let executorPrivateKey = await smc.getSecretValue({SecretId: 'nouns/ExecutorPrivateKey'}).promise();
+async function getEthereumPrivateKeys(secretsManager) {
+  let alchemyKey, executorPrivateKey;
+
+  let alchemyKeyPromise = secretsManager.getSecretValue({SecretId: 'nouns/AlchemyKey'}, (_, data) => {
+    alchemyKey = data.SecretString;
+  }).promise();
+
+  let executorKeyPromise = secretsManager.getSecretValue({SecretId: 'nouns/ExecutorPrivateKey'}, (_, data) => {
+    executorPrivateKey = data.SecretString;
+  }).promise();
+
+  await Promise.all([alchemyKeyPromise, executorKeyPromise]); // Pull in parallel for speed
 
   return { alchemyKey, executorPrivateKey };
 }
