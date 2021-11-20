fe59ba0d95a5b03441baa61ab754d4befe398f73
theforager <0xforager@gmail.com>
2021-11-20 01:52:36 -0600

    Make addr dynamic by chain, fix addresses

--- packages/webapp/src/config.ts
+++ packages/webapp/src/config.ts
@@ -1,8 +1,15 @@
 import { ChainId } from '@usedapp/core';
 import {providers, getDefaultProvider} from 'ethers';
 
+/** Select the Chain to Use */
+const USE_CHAIN_NAME = 'rinkeby';
+const USE_CHAIN_ID = ChainId.Rinkeby;
+/*--------------------------*/
+
 interface Config {
   auctionProxyAddress: string;
+  nounsDescriptor: string;
+  nounsSeeder: string;
   tokenAddress: string;
   nounsDaoProxyAddress: string;
   nounsDaoExecutorAddress: string;
@@ -28,6 +35,8 @@ const config: Record<SupportedChains, Config> = {
       process.env.REACT_APP_RINKEBY_WSRPC ||
       `wss://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_PROJECT_ID}`,
     auctionProxyAddress: '0x7cb0384b923280269b3BD85f0a7fEaB776588382',
+    nounsDescriptor: '0x53cB482c73655D2287AE3282AD1395F82e6a402F',
+    nounsSeeder : '0xA98A1b1Cc4f5746A753167BAf8e0C26AcBe42F2E',
     tokenAddress: '0x632f34c3aee991b10D4b421Bc05413a03d7a37eB',
     nounsDaoProxyAddress: '0xd1C753D9A23eb5c57e0d023e993B9bd4F5086b04',
     nounsDaoExecutorAddress: '0xd1C753D9A23eb5c57e0d023e993B9bd4F5086b04',
@@ -35,6 +44,8 @@ const config: Record<SupportedChains, Config> = {
   },
   [ChainId.Mainnet]: {
     auctionProxyAddress: '0x830BD73E4184ceF73443C15111a1DF14e495C706',
+    nounsDescriptor: '0x0Cfdb3Ba1694c2bb2CFACB0339ad7b1Ae5932B63',
+    nounsSeeder: '0xCC8a0FB5ab3C7132c1b2A0109142Fb112c4Ce515',
     tokenAddress: '0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03',
     nounsDaoProxyAddress: '0x6f3E6272A167e8AcCb32072d08E0957F9c79223d',
     nounsDaoExecutorAddress: '0x0BC3807Ec262cB779b38D65b38158acC3bfedE10',
@@ -50,6 +61,8 @@ const config: Record<SupportedChains, Config> = {
     auctionProxyAddress: '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853',
     tokenAddress: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
     // Temporarily set this to _any_ address until local deployment is configured
+    nounsDescriptor: '0x53cB482c73655D2287AE3282AD1395F82e6a402F',
+    nounsSeeder: '0xCC8a0FB5ab3C7132c1b2A0109142Fb112c4Ce515',    
     nounsDaoExecutorAddress: '0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6',
     nounsDaoProxyAddress: '0x610178dA211FEF7D417bC0e6FeD39F05609AD788',
     jsonRpcUri: 'http://localhost:8545',
@@ -58,6 +71,6 @@ const config: Record<SupportedChains, Config> = {
   },
 };
 
-export default config[ChainId.Mainnet];
+export default config[USE_CHAIN_ID];
 
-export const provider: providers.BaseProvider = getDefaultProvider();
+export const provider: providers.BaseProvider = getDefaultProvider(USE_CHAIN_NAME);
--- packages/webapp/src/wrappers/nounsSeeder.ts
+++ packages/webapp/src/wrappers/nounsSeeder.ts
@@ -1,7 +1,7 @@
-import { provider } from '../config';
 import { Contract } from 'ethers';
+import { default as config, provider } from '../config';
 
-const address: string = '0xCC8a0FB5ab3C7132c1b2A0109142Fb112c4Ce515';
+const address: string = config.nounsSeeder;
 
 const abi: string = '[{"inputs":[{"internalType":"uint256","name":"nounId","type":"uint256"},{"internalType":"contract INounsDescriptor","name":"descriptor","type":"address"}],"name":"generateSeed","outputs":[{"components":[{"internalType":"uint48","name":"background","type":"uint48"},{"internalType":"uint48","name":"body","type":"uint48"},{"internalType":"uint48","name":"accessory","type":"uint48"},{"internalType":"uint48","name":"head","type":"uint48"},{"internalType":"uint48","name":"glasses","type":"uint48"}],"internalType":"struct INounsSeeder.Seed","name":"","type":"tuple"}],"stateMutability":"view","type":"function"}]';
 
