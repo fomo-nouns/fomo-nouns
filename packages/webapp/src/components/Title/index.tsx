2786d7e78d6c9c5305f00ddec030cd7d17d9d000
theforager <0xforager@gmail.com>
2021-12-18 01:09:53 -0600

    Update titleTexts

--- packages/webapp/src/components/Title/index.tsx
+++ packages/webapp/src/components/Title/index.tsx
@@ -1,30 +1,32 @@
 import React from "react";
 import { useAppSelector } from "../../hooks";
 import classes from "./Title.module.css";
+import BlockTimer from '../BlockTimer';
 
-const activeAuctionCopy = `It's not Noun O'Clock yet
-Come back in:`;
-const gameActiveCopy = `Vote if we should mint this Noun!`;
-const attemptSettlementCopy = `Attempting
-to settle...`;
 
 const Title: React.FC<{}> = props => {
   const activeAuction = useAppSelector(state => state.auction.activeAuction);
   const attemptedSettle = useAppSelector(state => state.vote.attemptedSettle);
+  const votingActive = useAppSelector(state => state.vote.votingActive);
 
   let titleText = '';
   if (activeAuction) {
-    titleText = activeAuctionCopy;
+    titleText = `Come back at Noun O'Clock in:`;
   } else if (attemptedSettle) {
-    titleText = attemptSettlementCopy;
+    titleText = `Attempting to settle!`;
+  } else if (votingActive) {
+    titleText = `Vote if we should mint this Noun!`;
+  } else if (!activeAuction && !votingActive) {
+    titleText = `Times up! Waiting for next block...`;
   } else {
-    titleText = gameActiveCopy;
+    titleText = 'Loading FOMO Nouns...';
   }
 
   return (
     <div className={classes.Wrapper}>
       <h1 className={classes.Title}>
         {titleText}
+        {!activeAuction ? (<BlockTimer/>) : <></>}
       </h1>
     </div>
   )
