e443c9674e8bb1817ce64f4125006b396bd15f2d
theforager <0xforager@gmail.com>
2021-12-17 23:16:04 -0600

    Clean-up Title display

--- packages/webapp/src/components/Title/index.tsx
+++ packages/webapp/src/components/Title/index.tsx
@@ -4,23 +4,30 @@ import classes from "./Title.module.css";
 
 const activeAuctionCopy = `It's not Noun O'Clock yet
 Come back in:`;
-const gameActiveCopy = `Should we mint this Noun?`;
+const gameActiveCopy = `Vote if we should mint this Noun!`;
 const attemptSettlementCopy = `Attempting
 to settle...`;
-// const successCopy = `We minted a Noun!`;
-// const otherSettledCopy = `Someone else minted a Noun`;
 
 const Title: React.FC<{}> = props => {
-    const activeAuction = useAppSelector(state => state.auction.activeAuction);
-    const attemptedSettle = useAppSelector(state => state.vote.attemptedSettle);
-    return (
-        <div className={classes.Wrapper}>
-            {activeAuction && <h1 className={classes.Title}>{activeAuctionCopy}</h1>}
-            {!activeAuction && !attemptedSettle && <h1 className={classes.Title}>{gameActiveCopy}</h1>}
-            {attemptedSettle && <h1 className={classes.Title}>{attemptSettlementCopy}</h1>}
-        </div>
-    )
+  const activeAuction = useAppSelector(state => state.auction.activeAuction);
+  const attemptedSettle = useAppSelector(state => state.vote.attemptedSettle);
 
+  let titleText = '';
+  if (activeAuction) {
+    titleText = activeAuctionCopy;
+  } else if (attemptedSettle) {
+    titleText = attemptSettlementCopy;
+  } else {
+    titleText = gameActiveCopy;
+  }
+
+  return (
+    <div className={classes.Wrapper}>
+      <h1 className={classes.Title}>
+        {titleText}
+      </h1>
+    </div>
+  )
 };
 
 export default Title;
