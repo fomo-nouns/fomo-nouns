989fa9f86f6da0dd53a0d1208674cc5fce3cbfc2
theforager <0xforager@gmail.com>
2021-12-18 19:38:56 -0600

    Shorten title to be mobile-friendly

--- packages/webapp/src/components/Title/index.tsx
+++ packages/webapp/src/components/Title/index.tsx
@@ -12,7 +12,7 @@ const Title: React.FC<{}> = props => {
 
   let titleText = '';
   if (activeAuction) {
-    titleText = `It's not Noun O'Clock yet. Come back in:`;
+    titleText = `Come back at Noun O'Clock in:`;
   } else if (attemptedSettle) {
     titleText = `Attempting to settle...`;
   } else if (votingActive) {
