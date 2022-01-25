a76c6e6fa562db93dfc9899cc63088fc0a3b43eb
Nikita Goncharuk <nikitago2000@gmail.com>
2022-01-26 01:46:42 +0200

    Add auction data checks for notificationrequest

--- packages/mobileapp/notifications_backend/notificationrequest/app.js
+++ packages/mobileapp/notifications_backend/notificationrequest/app.js
@@ -1,44 +1,97 @@
 // SPDX-License-Identifier: MIT-0
 
 const AWS = require('aws-sdk');
+const axios = require('axios');
+
+const SUBGRAPH_API = 'https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph';
 
 const { STATE_MACHINE_ARN } = process.env;
 
 const sf = new AWS.StepFunctions();
 
-exports.handler = (event, context, callback) => {
-    let body = JSON.parse(event.body)
+exports.handler = async (event, context, callback) => {
+    // let body = JSON.parse(event.body);
+    let body = event.body;
 
-    const id = parseInt(body.previousAuctionId) + 1;
-    const endTimeUnix = parseInt(body.endTimeUnix);
+    var id;
+    var endTimeUnix;
+    var auctionSettled;
 
-    const payload = {
-        "auction": {
-            "id": id,
-            "endTimeUnix": endTimeUnix,
-            "toEndInTime": true,
-            "missedMarginally": false
-        }
-    };
+    try {
+        id = parseInt(body.previousAuctionId) + 1;
 
-    var sfParams = {
-        stateMachineArn: STATE_MACHINE_ARN,
-        input: JSON.stringify(payload)
-    };
+        const auctionData = await axios({
+            url: SUBGRAPH_API,
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: {
+                query: `
+                query{
+                    auction(id: ${id}) {
+                        endTime
+                        settled
+                    }
+                }`
+            }
+        });
+        endTimeUnix = parseInt(auctionData.data.data.auction.endTime);
+        auctionSettled = auctionData.data.data.auction.settled;
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                "notificationRequested": false,
+                "message": "Couldn't fetch data about auction",
+                "error": JSON.stringify(err),
+            })
+        };
 
-    sf.startExecution(sfParams, (err, data) => {
-        if (err) {
-            const response = {
-                statusCode: 500,
-                body: 'Failed to record notification request: ' + JSON.stringify(err)
-            };
-            callback(null, response);
-        } else {
-            const response = {
+    }
+
+    if (!auctionSettled) {
+        const payload = {
+            "auction": {
+                "id": id,
+                "endTimeUnix": endTimeUnix,
+                "toEndInTime": true,
+                "missedMarginally": false
+            }
+        };
+
+        var sfParams = {
+            stateMachineArn: STATE_MACHINE_ARN,
+            input: JSON.stringify(payload)
+        };
+
+        try {
+            await sf.startExecution(sfParams).promise();
+
+            return {
                 statusCode: 200,
-                body: 'Notification request recorded successfully'
+                body: JSON.stringify({
+                    "notificationRequested": true,
+                    "message": 'Notification request recorded successfully',
+                })
             };
-            callback(null, response);
+        } catch (err) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({
+                    "notificationRequested": false,
+                    "message": 'Failed to record notification request',
+                    "error": JSON.stringify(err),
+                })
+            };
         }
-    });
+    } else {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                "notificationRequested": false,
+                "message": 'Specified auction already ended',
+            })
+        };
+    }
 };
