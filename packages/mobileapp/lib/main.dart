81a720f9b9a34e51fbf47a99aa1401c1483f6860
Nikita Goncharuk <nikitago2000@gmail.com>
2022-02-18 23:55:28 +0200

    Added some code to debug notification recieveing

--- packages/mobileapp/lib/main.dart
+++ packages/mobileapp/lib/main.dart
@@ -3,18 +3,112 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:mobileapp/app/colors.dart';
 import 'package:mobileapp/app/const_names.dart';
 import 'package:mobileapp/screens/home_screen.dart';
 import 'package:notifications_repository/notifications_repository.dart';
 import 'firebase_options.dart';
 
+//TODO: remove class after end of notifications debug
+@HiveType(typeId: 0)
+class NotificationMessage {
+  @HiveField(0)
+  String? from;
+
+  @HiveField(1)
+  String? messageId;
+
+  @HiveField(2)
+  String? senderId;
+
+  @HiveField(3)
+  DateTime? sendTime;
+
+  @HiveField(4)
+  String? notificationTitle;
+
+  @HiveField(5)
+  String? notificationBody;
+}
+
+//TODO: remove class after end of notifications debug
+class NotificationMessageAdapter extends TypeAdapter<NotificationMessage> {
+  @override
+  final int typeId = 0;
+
+  @override
+  NotificationMessage read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return NotificationMessage()
+      ..from = fields[0] as String?
+      ..messageId = fields[1] as String?
+      ..senderId = fields[2] as String?
+      ..sendTime = fields[3] as DateTime?
+      ..notificationTitle = fields[4] as String?
+      ..notificationBody = fields[5] as String?;
+  }
+
+  @override
+  void write(BinaryWriter writer, NotificationMessage obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.from)
+      ..writeByte(1)
+      ..write(obj.messageId)
+      ..writeByte(2)
+      ..write(obj.senderId)
+      ..writeByte(3)
+      ..write(obj.sendTime)
+      ..writeByte(4)
+      ..write(obj.notificationTitle)
+      ..writeByte(5)
+      ..write(obj.notificationBody);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NotificationMessageAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-  // print("Handling a background message: ${message.messageId}");
+  //TODO: remove after end of notifications debug
+  await Hive.initFlutter();
+  if (!Hive.isAdapterRegistered(NotificationMessageAdapter().typeId)) {
+    Hive.registerAdapter(NotificationMessageAdapter());
+  }
+  late Box<NotificationMessage> box;
+  if (Hive.isBoxOpen("notifications_test")) {
+    box = Hive.box<NotificationMessage>('notifications_test');
+  } else {
+    box = await Hive.openBox<NotificationMessage>('notifications_test');
+  }
+  box.add(
+    NotificationMessage()
+      ..messageId = message.messageId
+      ..from = message.from
+      ..sendTime = message.sentTime
+      ..senderId = message.senderId
+      ..notificationTitle = message.notification?.title
+      ..notificationBody = message.notification?.body,
+  );
+  print("Handling a background message: ${message.messageId}");
 }
 
 /// Setting HeadsUp(foreground) notifications show
@@ -59,6 +153,11 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await Hive.initFlutter();
+  //TODO: remove registration after end of notifications debug
+  Hive.registerAdapter(NotificationMessageAdapter());
+  await Hive.openBox<NotificationMessage>('notifications_test');
 
   // final NotificationsRepository notificationsRepository =
   //     NotificationsRepository();
--- packages/mobileapp/lib/screens/wtf/wtf_screen.dart
+++ packages/mobileapp/lib/screens/wtf/wtf_screen.dart
@@ -1,15 +1,55 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:mobileapp/app/colors.dart';
 import 'package:mobileapp/screens/shared_widgets/helper.dart';
+
+import '../../main.dart';
 
 class WtfScreen extends StatelessWidget {
   const WtfScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialWidget(
-      child: Center(
-        child: Text("Wtf Screen"),
-      ),
-    );
+    return MaterialWidget(
+        child: ValueListenableBuilder<Box<NotificationMessage>>(
+      valueListenable:
+          Hive.box<NotificationMessage>('notifications_test').listenable(),
+      builder: (context, box, widget) {
+        return ListView.builder(
+          itemCount: box.length,
+          itemBuilder: (context, listIndex) {
+            return Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Container(
+                    height: 1,
+                    color: AppColors.black,
+                  ),
+                  Text(
+                      box.getAt(listIndex)?.notificationTitle ?? "error_title"),
+                  Text(
+                    box
+                            .getAt(listIndex)
+                            ?.sendTime
+                            ?.toLocal()
+                            .toIso8601String() ??
+                        "error",
+                  ),
+                  Text(box.getAt(listIndex)?.from ?? "error"),
+                  Text(box.getAt(listIndex)?.messageId ?? "error"),
+                  // Text(box.getAt(listIndex)?.notificationBody ?? "error"),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    )
+        // Center(
+        //   child: Text("Wtf Screen"),
+        // ),
+        );
   }
 }
