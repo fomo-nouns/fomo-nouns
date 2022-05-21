fd1c45fae72d4659eaefbe7318c565c20411ba41
ng <hey@nikitagoncharuk.com>
2022-05-21 18:55:25 +0300

    Show/hide stale players on click

--- packages/webapp/src/components/PlayersConnected/index.tsx
+++ packages/webapp/src/components/PlayersConnected/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useAppSelector } from "../../hooks";
 import classes from './PlayersConnected.module.css';
 
@@ -7,14 +8,18 @@ const PlayersConnected: React.FC<{}> = props => {
   const activeVoters = useAppSelector(state => state.vote.activeVoters);
   const voteConnected = useAppSelector(state => state.vote.connected);
 
+  const [showStalePlayers, setShowStalePlayers] = useState(false);
+
   if (!activeAuction && voteConnected) {
     return (
-      <div className={classes.Connected}>
+      <div 
+        onClick={() => setShowStalePlayers(!showStalePlayers)}
+        className={classes.Connected}>
         <span>
           Active Players: {activeVoters}
         </span>
-        <span className={classes.Stale}>
-          Stale: {numConnections - activeVoters}
+        <span className={showStalePlayers ? classes.Stale : classes.StaleHidden}>
+          · Stale: {numConnections - activeVoters}
         </span>
       </div>
     );
