295ea5b77e4d051ecc30de4962d79194f954fa55
ng <0xng@proton.me>
2022-07-06 06:20:05 +0300

    Add PT font and vote with keys text

--- packages/webapp/src/components/VoteWithKeys/index.tsx
+++ packages/webapp/src/components/VoteWithKeys/index.tsx
@@ -30,9 +30,18 @@ const VoteWithKeys:React.FC<{}> = (props) => {
 //     }    
 //   }
 
+  // TODO: show only if keyboard available
+
   return(
     <div className={classes.Wrapper}>
-        <p>Vote with keys</p>
+        <div className={classes.Text}>Vote with</div>
+        <svg className={classes.Key} width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12 12.3005V5.91192C12 5.1046 11.0927 4.63006 10.4296 5.09055L5.31875 8.63975C4.71065 9.06205 4.75632 9.97573 5.40351 10.3353L10.5144 13.1746C11.1809 13.5449 12 13.063 12 12.3005Z"/>
+        </svg>
+        <svg className={classes.Key} width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M6 5.69951L6 12.0881C6 12.8954 6.90729 13.3699 7.57039 12.9094L12.6812 9.36025C13.2894 8.93795 13.2437 8.02427 12.5965 7.66472L7.48564 4.82536C6.81911 4.45506 6 4.93703 6 5.69951Z"/>
+        </svg>
+        <div className={classes.Text}>keys</div>
     </div>
   );
 }
