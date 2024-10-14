ce3e29bd2ee9cc178f578bbd95822fa34d2cd78e
lastpunk9999 <83411264+lastpunk9999@users.noreply.github.com>
2024-10-14 11:54:42 -0400

    Update utils.js

--- packages/webapp/src/nouns-assets-package/utils.js
+++ packages/webapp/src/nouns-assets-package/utils.js
@@ -1,4 +1,4 @@
-"use strict";
+
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPartData = exports.getNounSeedFromBlockHash = exports.getPseudorandomPart = exports.shiftRightAndCast = exports.getRandomNounSeed = exports.getNounData = void 0;
 const solidity_1 = require("@ethersproject/solidity");
