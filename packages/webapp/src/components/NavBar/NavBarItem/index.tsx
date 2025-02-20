2ba6ba9ffab66da77977ca8bf8592c66fc846f65
theforager <0xforager@gmail.com>
2025-02-19 20:18:35 -0600

    feat: vibe code update to React 18

--- packages/webapp/src/components/NavBar/NavBarItem/index.tsx
+++ packages/webapp/src/components/NavBar/NavBarItem/index.tsx
@@ -1,11 +1,19 @@
 import classes from './NavBarItem.module.css';
+import { ReactNode } from 'react';
 
-const NavBarItem: React.FC<{ onClick?: () => void; className?: string }> = props => {
+interface NavBarItemProps {
+  onClick?: () => void;
+  className?: string;
+  children: ReactNode;
+}
+
+const NavBarItem: React.FC<NavBarItemProps> = props => {
   const { onClick, children, className } = props;
   return (
-    <div className={` ${classes.navBarItem} ${className}`} onClick={onClick}>
+    <div className={`${classes.navBarItem} ${className}`} onClick={onClick}>
       {children}
     </div>
   );
 };
+
 export default NavBarItem;
